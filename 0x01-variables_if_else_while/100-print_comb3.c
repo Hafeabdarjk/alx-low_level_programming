5fcef69bf70e85fec2935fcd66a63e4f22baf2e6
hafeabdarjk <apdohafez1256@gmail.com>
2024-02-06 04:49:27 +0200

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,28 +8,27 @@
  */
 int main(void)
 {
-	int c1 = 0;
-	int c2 = 1;
-
+	int c1;
+	int c2;
+	c1 = 0;
 	while (c1 < 9)
 	{
-		c2 = 1;
 		while (c2 < 10)
 		{
 			if (c2 > c1)
 			{
-			putchar(48 + c1);
-			putchar(48 + c2);
-			if (c1 != 8 && c2 != 9)
-			{
-			putchar(',');
-			putchar(' ');
+				putchar(48 + c1);
+				putchar(48 + c2);
+				if (c1 != 8 && c2 != 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
 			}
-			}
-		
 			c2++;
 		}
 		c1++;
+		c2 = 1;
 	}
 	putchar('\n');
 	return (0);
