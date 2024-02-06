f6d53a88a5577854f28525173572f85b52c867b8
hafeabdarjk <apdohafez1256@gmail.com>
2024-02-06 06:05:31 +0200

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int c1, c2, c3;
 
-	for(c1 = 0; c1 <= 7; c1++)
+	for (c1 = 0; c1 <= 7; c1++)
 	{
 		for (c2 = c1 + 1; c2 <= 8; c2++)
 		{
-			for (c3 = c2 +1; c3 <= 9; c3++)
+			for (c3 = c2 + 1; c3 <= 9; c3++)
 			{
 				putchar (48 + c1);
 				putchar (48 + c2);
